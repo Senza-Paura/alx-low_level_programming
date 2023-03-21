bd5d1cff0525859417e5c4bf4a121a59104aae58
Senza-Paura <othmantahir01@gmail.com>
2023-03-21 12:07:56 -0700

    104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,29 +1,25 @@
 #include <stdio.h>
-/**
- * main - prints the first 50 Fibonacci numbers, starting with 1 and 2
- *
- * Return: Always 0.
- */
-int main(void)
-{
-	int i;
-	unsigned long int num_a = 0;
-	unsigned long int num_b = 1;
-	unsigned long int temp = 0;
 
-	i = 0;
-	while (i < 98)
-	{
-		temp = num_a + num_b;
-		num_a = num_b;
-		num_b = temp;
-		printf("%lu", num_b);
-		if (i != 49)
-		{
-			printf(", ");
-		}
-		i++;
-	}
-	printf("\n");
-	return (0);
+int main()
+{
+    int a = 1, b = 2, c, i;
+
+    printf("%d, %d, ", a, b);
+
+    for (i = 3; i <= 98; i++)
+    {
+        c = a + b;
+        printf("%d", c);
+        if (i != 98)
+        {
+            printf(", ");
+        }
+        a = b;
+        b = c;
+    }
+
+    printf("\n");
+
+    return 0;
 }
+
